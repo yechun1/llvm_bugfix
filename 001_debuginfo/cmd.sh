a7a7092820f022e1e03db1cd1dbab3d1d8c4c933
Chris Ye <yechunliangcn@163.com>
2019-07-23 07:31:47 +0800

    update

--- 001_debuginfo/cmd.sh
+++ 001_debuginfo/cmd.sh
@@ -8,12 +8,15 @@ llvm-dis test.bc
 
 
 
-clang -ffunction-sections -fexceptions test.c -o test1.o
-clang -g -ffunction-sections -fexceptions test.c -o test2.o
+clang -ffunction-sections -fexceptions test.c -o test.o
+clang -ffunction-sections -fexceptions test.c -g -o test-g.o
 
+
+clang -c -O2 -mllvm -opt-bisect-limit=100 test.c -o test.o
+clang -c -O2 -mllvm -opt-bisect-limit=100 test.c -g -o test-g.o
 
 # Disassembly of section .text
 
-objdump -j .text -S test1.o
-objdump -j .text -S test2.o
+objdump -j .text -S test.o
+objdump -j .text -S test-g.o
 
