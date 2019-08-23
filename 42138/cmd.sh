a243e21c4798c06a35dbd3e1c008fed03d47b1ea
Chris Ye <yechunliangcn@163.com>
2019-08-23 17:13:03 +0800

    update

--- 42138/cmd.sh
+++ 42138/cmd.sh
@@ -1,28 +1,29 @@
 # ../../llvm-project/clang/utils/check_cfc/clang++ test.cc -w -c -O1 -o tmp.ll
 # clang++ PowerParser.ii.cc -c -O1
-# ../../llvm-project/clang/utils/check_cfc/clang++ test.cc -c -O1
+../../llvm-project/clang/utils/check_cfc/clang++ test.cc -c -O1
 
 
-## 132 vs 133
-##clang++ -mllvm -opt-bisect-limit=132 test.cc -c -O1 -o a.o
-##clang++ -mllvm -opt-bisect-limit=133 test.cc -c -O1 -o b.o
-#
-## 132 vs 137(g)
-##clang++ -mllvm -opt-bisect-limit=2 test.cc -c -O1 -o a.o
-##clang++ -mllvm -opt-bisect-limit=2 test.cc -c -O1 -g -o b.o
-#
-## 133 vs 137(g)
-##clang++ -mllvm -opt-bisect-limit=130 test.cc -c -O1 -o a.o
-##clang++ -mllvm -opt-bisect-limit=134 test.cc -c -O1 -g -o b.o
+# 132 vs 133
+#clang++ -mllvm -opt-bisect-limit=132 test.cc -c -O1 -o a.o
+#clang++ -mllvm -opt-bisect-limit=133 test.cc -c -O1 -o b.o
+
+# 132 vs 137(g)
+#clang++ -mllvm -opt-bisect-limit=2 test.cc -c -O1 -o a.o
+#clang++ -mllvm -opt-bisect-limit=2 test.cc -c -O1 -g -o b.o
+
+# 133 vs 137(g)
+#clang++ -mllvm -opt-bisect-limit=134 test.cc -c -O1 -o a.o
+#clang++ -mllvm -opt-bisect-limit=138 test.cc -c -O1 -g -o b.o
+
+# self
+#clang++ -mllvm -opt-bisect-limit=137 test.cc -c -O1 -g -o a.o
+#clang++ -mllvm -opt-bisect-limit=137 test.cc -c -O1 -g -o b.o -gmodules
+
+#objdump -d a.o > a.obj; objdump -d b.o > b.obj
+#rm -rf a.o b.o; colordiff a.obj b.obj
+
+# debug
 #clang++ -S -mllvm -opt-bisect-limit=133 test.cc -c -O1 -o - 1>a.o 2>&1
 #clang++ -S -mllvm -opt-bisect-limit=137 test.cc -c -O1 -g -o - 1>b.o 2>&1
-#
-## self
-##clang++ -mllvm -opt-bisect-limit=137 test.cc -c -O1 -g -o a.o
-##clang++ -mllvm -opt-bisect-limit=137 test.cc -c -O1 -g -o b.o -gmodules
-#
-##objdump -d a.o > a.obj; objdump -d b.o > b.obj
-##rm -rf a.o b.o; meld a.obj b.obj
-#
 #meld a.o b.o
 
--- 42138/cmd2.sh
+++ 42138/cmd2.sh
@@ -1,16 +1,12 @@
-# ../../llvm-project/clang/utils/check_cfc/clang++ PowerParser.cc -w -c -O1 -o tmp.ll
+# ../../llvm-project/clang/utils/check_cfc/clang++ test.cc -w -c -O1 -o tmp.ll
 # clang++ PowerParser.ii.cc -c -O1
-# ../../llvm-project/clang/utils/check_cfc/clang++ PowerParser.cc -c -O1
-
+# ../../llvm-project/clang/utils/check_cfc/clang++ test.cc -c -O1
 
 #NUM=$1
 
 #clang++ -g -w -O1 -S -emit-llvm test.cc -mllvm -opt-bisect-limit=$NUM -o test.ll
 #llc -stop-before=branch-folder test.ll -o - | grep unnamed
 #llc -o - test.mir -mtriple=x86_64-- -run-pass=branch-folder
-
-
-
 
 #llc -stop-after=machine-cp test.ll -o -
 #llc -stop-after=dead-mi-elimination test.ll -o -
@@ -24,10 +20,10 @@
 # -run-pass=
 
 
-clang++ -g -w -O1 -S -emit-llvm test.cc -o test.ll
-llc -stop-before=branch-folder test.ll -o test.mir
-llc -o - test.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck test.mir
-llc -o - branch-folder-skip-debug-instr.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck test.mir
+#clang++ -g -w -O1 -S -emit-llvm test.cc -o test.ll
+#llc -stop-before=branch-folder test.ll -o test.mir
+llc -o - g1.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck g1.mir
+#llc -o - branch-folder-skip-debug-instr.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck branch-folder-skip-debug-instr.mir
 
 
 ###########
@@ -35,13 +31,28 @@ llc -o - branch-folder-skip-debug-instr.mir -mtriple=x86_64-- -run-pass=branch-f
 ###########
 # error: test.mir:272:20: alloca instruction named '<unnamed alloca>' isn't defined in the function '_ZN1k1lEv'
 #  - { id: 0, name: '<unnamed alloca>', type: default, offset: -48, size: 1, 
-                   ^
+#                   ^
 # A: delete unnamed alloca code in MIR
+###########
+# error: a.mir:306:21: expected a machine operand
+#     CFI_INSTRUCTION <unserializable cfi directive>, debug-location !72
+###########
+# error: YAML:304:78: unknown enumerated scalar
+#  - { id: 0, type: spill-slot, offset: -40, size: 8, alignment: 8, stack-id: 0,
 ###########
 
 
 
 #debug
-#clang++ -w -S -emit-llvm test.cc -mllvm -opt-bisect-limit=0 -o test.ll
-#llc -stop-before=branch-folder test.ll -opt-bisect-limit=0 -o test.mir
-#llc -o - test.mir -mtriple=x86_64-- -run-pass=branch-folder
+# clang++ -S -emit-llvm test.cc -o test.ll
+# llc -stop-before=branch-folder test.ll -opt-bisect-limit=0 -o test.mir
+# llc -o - test.mir -mtriple=x86_64-- -run-pass=branch-folder
+# clang++ -S test.cc a
+
+#clang++ -g -w -O1 -S -emit-llvm test.cc -o a.ll
+#llc -stop-before=branch-folder a.ll -o a.mir
+#llc -o - a.mir -mtriple=x86_64-- -run-pass=branch-folder
+
+
+#llc -stop-after=branch-folder test.ll -o b
+#vimdiff a b
