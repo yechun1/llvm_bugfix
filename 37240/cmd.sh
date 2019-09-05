736eae6b3e2196a660dfba790ec94319dabf628b
Chris Ye <yechunliangcn@163.com>
2019-09-05 17:09:15 +0800

    a

--- 37240/cmd.sh
+++ 37240/cmd.sh
@@ -1,10 +1,10 @@
-opt --strip-debug test.ll -S > test2.ll
+opt --strip-debug test.ll -S > test1.ll
 
 #############
 # debug1
 #############
-#opt --strip-debug test.ll -S > test2.ll
-#llc test2.ll $1 -o test2.s
+#opt --strip-debug test.ll -S > test1.ll
+#llc test1.ll $1 -o test1.s
 #opt example.ll -S | llc -o b.s
 
 
@@ -15,18 +15,18 @@ opt --strip-debug test.ll -S > test2.ll
 #OPT="-pre-RA-sched=default"
 
 #llc test.ll -opt-bisect-limit=$NUM -o test.s
-#llc test.ll -o test2.s
-#llc test2.ll $1 -o test2.s
+#llc test.ll -o test1.s
+#llc test1.ll $1 -o test1.s
 #opt example.ll -S | llc -o b.s
-#vimdiff test.s test2.s
+#vimdiff test.s test1.s
 
 #############
 # debug2
 #############
 #NUM=$1
-#llc test2.ll -opt-bisect-limit=$NUM -o test.s
-#llc test2.ll -opt-bisect-limit=$(($NUM+1)) -o test2.s
-#colordiff test.s test2.s
+#llc test1.ll -opt-bisect-limit=$NUM -o test.s
+#llc test1.ll -opt-bisect-limit=$(($NUM+1)) -o test1.s
+#colordiff test.s test1.s
 
 
 #############
@@ -34,17 +34,19 @@ opt --strip-debug test.ll -S > test2.ll
 #############
 #NUM=$1
 #llc test.ll -opt-bisect-limit=$NUM -o test.s
-#llc test2.ll -opt-bisect-limit=$NUM -o test2.s
-#colordiff test.s test2.s
+#llc test1.ll -opt-bisect-limit=$NUM -o test1.s
+#colordiff test.s test1.s
 #############
 
 ### debug4 ###
-#llc test.ll -stop-before=postmisched -o test.mir
+llc test.ll -stop-before=postmisched -o test.mir
+llc test1.ll -stop-before=postmisched -o test1.mir
 #llc test.mir -run-pass=postmisched -o test.s
-#llc test2.ll -stop-before=postmisched -o test2.mir
-#llc test2.mir -run-pass=postmisched -o test2.s
-#colordiff test.s test2.s
+#llc test1.mir -run-pass=postmisched -o test1.s
+#colordiff test.s test1.s
 
 ### debug5 ###
-llc test.mir -run-pass=postmisched -misched-dcpl -debug-only='machine-scheduler' -o - 1>test.log 2>&1
-llc test2.mir -run-pass=postmisched -misched-dcpl -debug-only='machine-scheduler' -o - 1>test2.log 2>&1
+#llc test.mir -run-pass=postmisched -misched-dcpl -debug-only='machine-scheduler' -o - 1>test.log 2>&1
+#llc test1.mir -run-pass=postmisched -misched-dcpl -debug-only='machine-scheduler' -o - 1>test1.log 2>&1
+llc test.mir -run-pass=postmisched -o - 1>test.log 2>&1
+llc test1.mir -run-pass=postmisched -o - 1>test1.log 2>&1
