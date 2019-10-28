b2856aa42d8e28d49aadcf809fa340ab90a9267e
Chris Ye <yechunliangcn@163.com>
2019-10-28 18:22:52 +0800

    43758

--- 43758/cmd.sh
+++ 43758/cmd.sh
@@ -2,32 +2,20 @@
 PATH=$PATH:/data/proj/compiler/llvm/llvm-project/build/bin
 
 ### Reproduce
-/home/chris/llvm-project/clang/utils/check_cfc/clang++ /home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp -w -c -O2 -o tmp.ll 2>/dev/null
+#/home/chris/llvm-project/clang/utils/check_cfc/clang++ /home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp -w -c -O2 -o tmp.ll 2>/dev/null
 
 
-### Reproduce 2
-#rm -rf out* log*
+### Simplify Reproduce
+rm -rf out* log*
 #clang++ /home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp -S -O2 -mllvm -opt-bisect-limit=317 -o out1.o 2>log1
 #clang++ /home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp -S -O2 -mllvm -opt-bisect-limit=320 -o out2.o -g 2>log2
 #objdump -d -j .text out1.o > out1.obj
 #objdump -d -j .text out2.o > out2.obj
 #diff -u out1.obj out2.obj
-#rm -rf out*.o
+#rm -rf out* test* log* tmp*
 
 
-### debug 1
-
-#clang++ /home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp -S -O2 -mllvm -opt-bisect-limit=317 -o - 1>out1.ll 2>&1
-#clang++ -g /home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp -S -O2 -mllvm -opt-bisect-limit=320 -o - 1>out2.ll 2>&1
-#FILE=/home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp
-#clang++ $FILE -S -O2 -mllvm -opt-bisect-limit=316 -o out1.ll 2>log1
-#clang++ $FILE -S -O2 -mllvm -opt-bisect-limit=319 -o out2.ll -g 2>log2
-#clang++ $FILE -S -O2 -mllvm -opt-bisect-limit=317 -o out1.ll 2>log1
-#clang++ $FILE -S -O2 -mllvm -opt-bisect-limit=320 -o out2.ll -g 2>log2
-
-### debug 2
-
-
+### Analyze1
 #for ((i=317; i>=216; i--))
 #do
 #  NUM=$i
@@ -40,51 +28,37 @@ PATH=$PATH:/data/proj/compiler/llvm/llvm-project/build/bin
 #  colordiff out1.obj out2.obj
 #  echo ""
 #done
-#
-#for ((i=212; i>=0; i--))
-#do
-#  NUM=$i
-#  echo "################[$NUM]################"
-#  clang++ -c -O2 $FILE -I$INCLUDE -mllvm -opt-bisect-limit=$NUM -o out1.o 2>log1
-#  clang++ -c -g -O2 $FILE -I$INCLUDE -mllvm -opt-bisect-limit=$NUM -o out2.o 2>log2
-#  objdump -d -j .text out1.o > out1.obj
-#  objdump -d -j .text out2.o > out2.obj
-#  rm -rf out*.o
-#  colordiff out1.obj out2.obj
-#done
-
-#clang++ -c -O2 $FILE -I$INCLUDE -mllvm -opt-bisect-limit=317 -o out1.o 2>log1
-#clang++ -c -g -O2 $FILE -I$INCLUDE -mllvm -opt-bisect-limit=320 -o out2.o 2>log2
-#objdump -d -j .text out1.o > out1.obj
-#objdump -d -j .text out2.o > out2.obj
-##rm -rf out*.o
-#colordiff out1.obj out2.obj
 
 
-#rm -rf out1/* out2/*
-
-### debug1
-#for ((i=270; i>=216; i--))
-#do
-#  NUM=$i
-#  echo "################[$NUM/ $((NUM-3))]################"
-#  clang++ -O2 -S -emit-llvm $FILE -I$INCLUDE -mllvm -opt-bisect-limit=$((NUM-3)) -o out1/out${NUM}.ll
-#  clang++ -O2 -g -S -emit-llvm $FILE -I$INCLUDE -mllvm -opt-bisect-limit=$NUM -o - | opt -S -strip-debug -o out2/out${NUM}.ll
-#  #colordiff out1.ll out2.ll
-#  echo ""
-#done
-#
-#for ((i=212; i>=0; i--))
-#do
-#  NUM=$i
-#  echo "################[$NUM]################"
-#  clang++ -O2 -S -emit-llvm $FILE -I$INCLUDE -mllvm -opt-bisect-limit=$NUM -o out1/out${NUM}.ll
-#  clang++ -O2 -g -S -emit-llvm $FILE -I$INCLUDE -mllvm -opt-bisect-limit=$NUM -o - | opt -S -strip-debug -o out2/out${NUM}.ll
-#done
+### Debug 1
+#FILE=/home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp
+#clang++ -S $FILE -O2 -mllvm -opt-bisect-limit=317 -o out1.ll 2>log1
+#clang++ -S $FILE -O2 -mllvm -opt-bisect-limit=320 -o out2.ll -g 2>log2
 
 
+### Debug2 - convert to ll
+FILE=/home/chris/test-suite/MultiSource/Benchmarks/DOE-ProxyApps-C++/HPCCG/generate_matrix.cpp
+clang++ -S $FILE -O2 -mllvm -opt-bisect-limit=319 -o test1.s -g 2>ref1
+clang++ -S $FILE -O2 -mllvm -opt-bisect-limit=320 -o test2.s -g 2>ref2
 
 
+### Debug3 - design a test case based on leaFixup64.mir
+#llc example.mir -stop-before=x86-fixup-LEAs -o tmp1.mir
+#llc tmp1.mir -run-pass x86-fixup-LEAs -o out1.mir 2>log1
+llc example1.mir -run-pass x86-fixup-LEAs -mcpu=corei7-avx -o out1.mir 2>log1
+llc example2.mir -run-pass x86-fixup-LEAs -mcpu=corei7-avx -o out2.mir 2>log2
 
+#llvm-mca -mtriple=x86_64-unknown-unknown -mcpu=x86-64 -instruction-tables test1.s
+#llvm-mc test1.s -filetype obj -triple -o out.o
+# RUN: llvm-mc %s -filetype obj -triple armv7a-- -o %t.o
 
+#llvm-mca -mtriple=x86_64-unknown-unknown -mcpu=btver2 -all-views=false -instruction-info
+# RUN: not llvm-cfi-verify %t.o 2>&1 | FileCheck %s
+
+#clang++ $FILE -O2 -S -o example.ll -g
+
+#clang++ $FILE -O0 -S -emit-llvm -o example.ll -g
+#llc test1.ll -stop-before=x86-fixup-LEAs -o tmp1.mir
+#llc test2.ll -stop-before=x86-fixup-LEAs -o tmp2.mir
+#llc tmp1.mir -run-pass=x86-fixup-LEAs -o out1.mir
 
