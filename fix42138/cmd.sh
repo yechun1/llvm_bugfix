63db841f5e40fb7afc0e2259acfd4c65e61fa735
Chris Ye <yechunliangcn@163.com>
2019-11-13 10:15:36 +0800

    42138

--- fix42138/cmd.sh
+++ fix42138/cmd.sh
@@ -1,29 +1,43 @@
-# ../../llvm-project/clang/utils/check_cfc/clang++ test.cc -w -c -O1 -o tmp.ll
-# clang++ PowerParser.ii.cc -c -O1
-#../../llvm-project/clang/utils/check_cfc/clang++ test.cc -c -O1
+# https://bugs.llvm.org/show_bug.cgi?id=42138
+export PATH=/data/proj/compiler/llvm/llvm-project/build/bin:$PATH
 
+### reproduce
+/home/chris/llvm-project/clang/utils/check_cfc/clang++ test.cc -w -c -O1 -o tmp.ll
 
-# 132 vs 133
-#clang++ -mllvm -opt-bisect-limit=132 test.cc -c -O1 -o a.o
-#clang++ -mllvm -opt-bisect-limit=133 test.cc -c -O1 -o b.o
+### reproduce reduce1
+#clang++ test.cc -c -O1 -o out1.o
+#clang++ test.cc -c -O1 -o out2.o -g
+#objdump -d out1.o > out1.obj; objdump -d out2.o > out2.obj
+#rm -rf out1.o out2.o; colordiff out1.obj out2.obj
 
-# 132 vs 137(g)
-#clang++ -mllvm -opt-bisect-limit=2 test.cc -c -O1 -o a.o
-#clang++ -mllvm -opt-bisect-limit=2 test.cc -c -O1 -g -o b.o
+### reproduce reduce2
+# clang++ test.cc -S -O1 -o out1.s
+# clang++ test.cc -S -O1 -o out2.s -g
 
-# 133 vs 137(g)
-#clang++ -mllvm -opt-bisect-limit=134 test.cc -c -O1 -o a.o
-#clang++ -mllvm -opt-bisect-limit=138 test.cc -c -O1 -g -o b.o
+### analyze1
+#clang++ -mllvm -opt-bisect-limit=134 test.cc -c -O1 -o out1.o
+#clang++ -mllvm -opt-bisect-limit=138 test.cc -c -O1 -o out2.o -g
+#objdump -d out1.o > out1.obj; objdump -d out2.o > out2.obj
+#rm -rf out1.o out2.o; colordiff out1.obj out2.obj
 
-# self
-#clang++ -mllvm -opt-bisect-limit=137 test.cc -c -O1 -g -o a.o
-#clang++ -mllvm -opt-bisect-limit=137 test.cc -c -O1 -g -o b.o -gmodules
+### analyze2
+#rm -rf out*
+#NUM=138
+#clang++ -mllvm -opt-bisect-limit=$((NUM-4)) test.cc -c -O1 -o out1.o 2>log1
+#clang++ -mllvm -opt-bisect-limit=$NUM test.cc -c -O1 -o out2.o -g 2>log2
+#objdump -d out1.o > out1.obj; objdump -d out2.o > out2.obj
+#rm -rf out1.o out2.o; colordiff out1.obj out2.obj
 
-#objdump -d a.o > a.obj; objdump -d b.o > b.obj
-#rm -rf a.o b.o; colordiff a.obj b.obj
+### analyze3 self compare
+rm -rf out*
+#NUM=130
+#clang++ -mllvm -opt-bisect-limit=$((NUM-1)) test.cc -c -O1 -o out1.o  2>log1
+#clang++ -mllvm -opt-bisect-limit=$NUM test.cc -c -O1 -o out2.o  2>log2
+#objdump -d out1.o > out1.obj; objdump -d out2.o > out2.obj
+#rm -rf out1.o out2.o; colordiff out1.obj out2.obj
+
 
 # debug
-clang++ -S -mllvm -opt-bisect-limit=133 test.cc -c -O1 -o - 1>a.o 2>&1
-clang++ -S -mllvm -opt-bisect-limit=137 test.cc -c -O1 -g -o - 1>b.o 2>&1
-colordiff a.o b.o
+#clang++ -S -mllvm -opt-bisect-limit=130 test.cc -c -O1 -o - 1>out1.s 2>&1
+#clang++ -S -mllvm -opt-bisect-limit=134 test.cc -c -O1 -g -o - 1>out2.s 2>&1
 
--- fix42138/cmd2.sh
+++ fix42138/cmd2.sh
@@ -1,9 +1,31 @@
-# ../../llvm-project/clang/utils/check_cfc/clang++ test.cc -w -c -O1 -o tmp.ll
-# clang++ PowerParser.ii.cc -c -O1
-#../../llvm-project/clang/utils/check_cfc/clang++ test.cc -c -O1
+export PATH=/data/proj/compiler/llvm/llvm-project/build/bin:$PATH
+#export PATH=/data/proj/compiler/llvm/llvm-project.release/build/bin:$PATH
 
-#llvm-lit ../../llvm-project/llvm/test/CodeGen/MIR/X86/branch-folder-with-debug.mir
-#llvm-lit ../../llvm-project.release/llvm/test/CodeGen/MIR/X86/branch-folder-with-debug.mir
+## run test1
+#llvm-lit /data/proj/compiler/llvm/llvm-project/llvm/test/CodeGen/MIR/X86/branch-folder-with-debug.mir
+#llvm-lit -vv /data/proj/compiler/llvm/llvm-project/llvm/test/CodeGen/MIR/X86/test.mir
+#llvm-lit -vv /data/proj/compiler/llvm/llvm-project.release/llvm/test/CodeGen/MIR/X86/test.mir
+
+
+## simplify
+## create origin a.mir and simplify the testcase
+# clang++ -w -O1 -S -emit-llvm test.cc -o a.ll
+# clang++ -w -O1 -S -emit-llvm test.cc -o b.ll -g
+#llc -stop-before=branch-folder -simplify-mir a.ll -o a.mir
+#llc -stop-before=branch-folder -simplify-mir b.ll -o b.mir
+#llc -o - a.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck a.mir
+
+## run issue1
+#llc -o - branch-folder-with-debug.mir -mtriple=x86_64-- -run-pass=branch-folder > tmp.mir
+#llc -o - branch-folder-with-debug.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck branch-folder-with-debug.mir
+#llc -o - a.mir -mtriple=x86_64-- -run-pass=branch-folder 1>log1 2>&1
+#llc -o - b.mir -mtriple=x86_64-- -run-pass=branch-folder 1>log2 2>&1
+
+## run issue2
+#llc -O3 -o - test2.mir -mtriple=x86_64-- -run-pass=branch-folder > tmp.mir
+#llc -O3 -o - test2.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck test.mir
+llc -O3 -o - test.mir -mtriple=x86_64-- -run-pass=branch-folder 1>log1 2>&1
+llc -O3 -o - test2.mir -mtriple=x86_64-- -run-pass=branch-folder 1>log2 2>&1
 
 #NUM=$1
 
@@ -22,15 +44,6 @@
 # -stop-before=
 # -run-pass=
 
-
-#clang++ -w -O1 -S -emit-llvm test.cc -o a.ll
-#llc -stop-before=branch-folder -simplify-mir a.ll -o b.mir
-#clang++ -g -w -O1 -S -emit-llvm test.cc -o b.ll
-#llc -stop-before=branch-folder -simplify-mir a.ll -o b.mir
-#llc -o - a.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck a.mir
-llc -o - branch-folder-with-debug.mir -mtriple=x86_64-- -run-pass=branch-folder > tmp.mir
-llc -o - branch-folder-with-debug.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck branch-folder-with-debug.mir
-
 #clang++ -w -O1 -S -emit-llvm test.cc -o a.ll
 #llc -stop-before=branch-folder a.ll -o a.mir
 #llc -o - a.mir -mtriple=x86_64-- -run-pass=branch-folder > a1.mir
@@ -39,6 +52,7 @@ llc -o - branch-folder-with-debug.mir -mtriple=x86_64-- -run-pass=branch-folder 
 #llc -stop-before=branch-folder b.ll -o b.mir
 #llc -o - b.mir -mtriple=x86_64-- -run-pass=branch-folder > b1.mir
 #llc -o - b.mir -mtriple=x86_64-- -run-pass=branch-folder | FileCheck b.mir
+
 
 
 ###########
@@ -68,3 +82,23 @@ llc -o - branch-folder-with-debug.mir -mtriple=x86_64-- -run-pass=branch-folder 
 
 #llc -stop-after=branch-folder test.ll -o b
 #vimdiff a b
+
+###########
+# error
+###########
+# error: test.mir:272:20: alloca instruction named '<unnamed alloca>' isn't defined in the function '_ZN1k1lEv'
+#  - { id: 0, name: '<unnamed alloca>', type: default, offset: -48, size: 1, 
+#                   ^
+# A: delete unnamed alloca code in MIR
+###########
+# error: a.mir:306:21: expected a machine operand
+#     CFI_INSTRUCTION <unserializable cfi directive>, debug-location !72
+###########
+# error: YAML:304:78: unknown enumerated scalar
+#  - { id: 0, type: spill-slot, offset: -40, size: 8, alignment: 8, stack-id: 0,
+###########
+
+
+
+#clang++ test.cc -emit-llvm -O1 -Xclang -disable-llvm-passes -c -o foo.bc
+#opt -O1 -debug-pass=Arguments foo.bc -disable-output
